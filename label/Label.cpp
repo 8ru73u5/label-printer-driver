bdc06e66ab663907abf993acff3526b451ae7281
8ru73u5 <brute.us@protonmail.com>
2020-02-29 02:29:39 +0100

    Remove unsupported label formats Add getters for protected members and remove friend class declaration

--- label/Label.cpp
+++ label/Label.cpp
@@ -2,11 +2,11 @@
 
 #include "Label.h"
 
-LabelType Label::label_type = LabelType::UNDEFINED;
+LabelType Label::type = LabelType::UNDEFINED;
 LabelDimensions Label::dimensions = {};
 
 Label::Label() {
-    if(Label::label_type == LabelType::UNDEFINED)
+    if(!is_valid())
         throw std::runtime_error("Label type not set! Use provided static function to set it");
 }
 
@@ -15,7 +15,7 @@ void Label::set_die_cut_label_type(LabelSubtypes::DieCut _label_type, const bool
     if(lab_it == LabelSubtypes::__die_cut_dimensions.end())
         throw std::runtime_error("This label type is not supported");
 
-    Label::label_type = LabelType::DIE_CUT;
+    Label::type = LabelType::DIE_CUT;
     Label::dimensions = lab_it->second;
 
     if(high_quality)
@@ -27,7 +27,7 @@ void Label::set_continuous_length_label_type(LabelSubtypes::ContinuousLength _la
     if(lab_it == LabelSubtypes::__continuous_length_dimensions.end())
         throw std::runtime_error("This label type is not supported");
 
-    Label::label_type = LabelType::CONTINUOUS_LENGTH;
+    Label::type = LabelType::CONTINUOUS_LENGTH;
     Label::dimensions.height_mm = lab_it->second.height_mm;
     Label::dimensions.width_mm = width_mm;
     Label::dimensions.width_pt = static_cast<uint32_t>(round(width_mm * 0.03937 * 300));
@@ -37,4 +37,16 @@ void Label::set_continuous_length_label_type(LabelSubtypes::ContinuousLength _la
 
     if(Label::dimensions.width_pt < 150 || Label::dimensions.width_pt > 11811)
         throw std::invalid_argument("Label width must be in range <150, 11811> points");
+}
+
+LabelType Label::get_type() {
+    return type;
+}
+
+LabelDimensions Label::get_dimensions() {
+    return dimensions;
+}
+
+bool Label::is_valid() {
+    return type != LabelType::UNDEFINED;
 }
--- label/Label.h
+++ label/Label.h
@@ -20,14 +20,14 @@ enum class LabelType {
 
 namespace LabelSubtypes {
     enum class ContinuousLength : int {
-        CL_12 = 257, CL_29 = 258, CL_38  = 264, CL_50 = 262,
-        CL_54 = 261, CL_62 = 259, CL_102 = 260
+        CL_12 = 257, CL_29 = 258, CL_38 = 264, CL_50 = 262,
+        CL_54 = 261, CL_62 = 259
     };
 
     enum class DieCut : int {
-        DC_17x54  = 269, DC_17x87  = 270, DC_23x23   = 370, DC_29x90 = 271,
-        DC_38x90  = 272, DC_39x48  = 367, DC_52x29   = 374, DC_62x29 = 274,
-        DC_62x100 = 275, DC_102x51 = 365, DC_102x152 = 366
+        DC_17x54  = 269, DC_17x87 = 270, DC_23x23 = 370, DC_29x90 = 271,
+        DC_38x90  = 272, DC_39x48 = 367, DC_52x29 = 374, DC_62x29 = 274,
+        DC_62x100 = 275
     };
 
     const std::map<ContinuousLength, LabelDimensions> __continuous_length_dimensions {
@@ -36,8 +36,7 @@ namespace LabelSubtypes {
             {ContinuousLength::CL_38,  { 0,  38, 0, 413  }},
             {ContinuousLength::CL_50,  { 0,  50, 0, 554  }},
             {ContinuousLength::CL_54,  { 0,  54, 0, 590  }},
-            {ContinuousLength::CL_62,  { 0,  62, 0, 696  }},
-            {ContinuousLength::CL_102, { 0, 102, 0, 1164 }}
+            {ContinuousLength::CL_62,  { 0,  62, 0, 696  }}
     };
 
     const std::map<DieCut, LabelDimensions> __die_cut_dimensions {
@@ -49,15 +48,13 @@ namespace LabelSubtypes {
             {LabelSubtypes::DieCut::DC_39x48,   { 48,   39,  495, 425  }},
             {LabelSubtypes::DieCut::DC_52x29,   { 29,   52,  271, 578  }},
             {LabelSubtypes::DieCut::DC_62x29,   { 29,   62,  271, 696  }},
-            {LabelSubtypes::DieCut::DC_62x100,  { 100,  62, 1109, 696  }},
-            {LabelSubtypes::DieCut::DC_102x51,  { 51,  102,  526, 1164 }},
-            {LabelSubtypes::DieCut::DC_102x152, { 152, 102, 1660, 1164 }}
+            {LabelSubtypes::DieCut::DC_62x100,  { 100,  62, 1109, 696  }}
     };
 }
 
 class Label {
 protected:
-    static LabelType label_type;
+    static LabelType type;
     static LabelDimensions dimensions;
 
 public:
@@ -67,9 +64,12 @@ public:
     static void set_die_cut_label_type(LabelSubtypes::DieCut label_type, bool high_quality = false);
     static void set_continuous_length_label_type(LabelSubtypes::ContinuousLength label_type, int width_mm, bool high_quality = false);
 
-    [[nodiscard]] virtual std::vector<uint8_t> get_printing_data() const = 0;
+    static LabelType get_type();
+    static LabelDimensions get_dimensions();
 
-    friend class PrinterJobData;  // Access label dimensions
+    static bool is_valid();
+
+    [[nodiscard]] virtual std::vector<uint8_t> get_printing_data() const = 0;
 };
 
 
