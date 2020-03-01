b6b2b84811e156fa3df37764fcd3fb2d039469b2
8ru73u5 <brute.us@protonmail.com>
2020-03-01 19:21:24 +0100

    Remove possibility to specify 600dpi quality
    Add docs

--- printer/PrinterJobData.cpp
+++ printer/PrinterJobData.cpp
@@ -1,6 +1,7 @@
+#include <cmath>
 #include "PrinterJobData.h"
 
-PrinterJobData::PrinterJobData() noexcept {
+PrinterJobData::PrinterJobData() {
     if(!Label::is_valid())
         throw std::runtime_error("Label type not set! Use provided static function to set it");
 
@@ -11,11 +12,9 @@ PrinterJobData::PrinterJobData() noexcept {
     raster_number = dimensions.width_pt;
 
     starting_page = true;
-    auto_cut = false;
-    cut_every_x_labels = 0;
+    cut_every_x_labels = {};
     cut_at_end = true;
     high_quality = false;
-    print_600dpi = false;
     margin_amount = label_type == LabelType::DIE_CUT ? 0 : 35;
 }
 
@@ -23,11 +22,9 @@ void PrinterJobData::set_is_starting_page(const bool _starting_page) noexcept {
     starting_page = _starting_page;
 }
 
-void PrinterJobData::set_auto_cut_options(const bool _auto_cut, const uint8_t _cut_every_x_labels) noexcept {
-    if(!_auto_cut && _cut_every_x_labels != 0)
-        throw std::invalid_argument("Cut every X labels option is only available when auto cut is set to true");
-
-    auto_cut = _auto_cut;
+void PrinterJobData::set_auto_cut_options(const std::optional<uint8_t> _cut_every_x_labels) {
+    if(_cut_every_x_labels && _cut_every_x_labels.value() == 0)
+        throw std::invalid_argument("Cut every x labels must be in range (0, 255>");
     cut_every_x_labels = _cut_every_x_labels;
 }
 
@@ -37,6 +34,16 @@ void PrinterJobData::set_cut_at_end(const bool _cut_at_end) noexcept {
 
 void PrinterJobData::set_quality(const bool _high_quality) noexcept {
     high_quality = _high_quality;
+}
+
+void PrinterJobData::set_margin_amount(const uint8_t margin_amount_mm) {
+    if(label_type == LabelType::DIE_CUT)
+        throw std::runtime_error("Margin for die-cut labels can't be set");
+
+    if(margin_amount_mm < 3 || margin_amount_mm > 127)
+        throw std::invalid_argument("Margin amount must be in range <3mm, 127mm>");
+
+    margin_amount = std::round(margin_amount_mm * 0.03937 * 300);
 }
 
 std::vector<uint8_t> PrinterJobData::construct_job_data_message() const noexcept {
@@ -61,31 +68,31 @@ std::vector<uint8_t> PrinterJobData::construct_job_data_message() const noexcept
     /* Set auto cut */
     std::array<uint8_t, 4> set_auto_cut_mode {
         0x1b, 0x69, 0x4d,
-        static_cast<uint8_t>(auto_cut ? 0x40 : 0x00)
+        static_cast<uint8_t>(cut_every_x_labels ? 0x40 : 0x00)
     };
     job_data.insert(job_data.end(), set_auto_cut_mode.begin(), set_auto_cut_mode.end());
 
     /* Set cut every x labels option */
-    if(auto_cut && cut_every_x_labels != 0) {
+    if(cut_every_x_labels) {
         std::array<uint8_t, 4> set_cut_every_opt{
             0x1b, 0x69, 0x41,
-            cut_every_x_labels
+            cut_every_x_labels.value()
         };
         job_data.insert(job_data.end(), set_cut_every_opt.begin(), set_cut_every_opt.end());
     }
 
-    /* Set expanded mode (cut at end and 600dpi printing) */
+    /* Set expanded mode (cut at end) */
     std::array<uint8_t, 4> set_exp_mode {
         0x1b, 0x69, 0x4b,
-        static_cast<uint8_t>(static_cast<uint8_t>(cut_at_end << 3u) | static_cast<uint8_t>(print_600dpi << 6u))
+        static_cast<uint8_t>(cut_at_end << 3u)
     };
     job_data.insert(job_data.end(), set_exp_mode.begin(), set_exp_mode.end());
 
     /* Set margin amount */
     std::array<uint8_t, 5> set_margin {
         0x1b, 0x69, 0x64,
-        static_cast<uint8_t>(margin_amount & 0xff),
-        static_cast<uint8_t>((margin_amount >> 8u) & 0xff)
+        static_cast<uint8_t>(margin_amount & 0xffu),
+        static_cast<uint8_t>((margin_amount >> 8u) & 0xffu)
     };
     job_data.insert(job_data.end(), set_margin.begin(), set_margin.end());
 
--- printer/PrinterJobData.h
+++ printer/PrinterJobData.h
@@ -1,36 +1,71 @@
 #ifndef LABEL_PRINTER_DRIVER_PRINTERJOBDATA_H
 #define LABEL_PRINTER_DRIVER_PRINTERJOBDATA_H
 
-#include <vector>
-#include <cinttypes>
 #include "../label/Label.h"
 
+/**
+ * Representation of a job data message that is sent
+ * to the printer before printing of each page.
+ */
 class PrinterJobData {
 private:
     LabelType label_type;
-    uint8_t label_width;
-    uint8_t label_height;
-    uint32_t raster_number;
+    uint8_t label_width;  /**< Width of the label in millimeters */
+    uint8_t label_height;  /**< Height of the label in millimeters */
+    uint32_t raster_number;  /**< Width of the label in pixels (points) */
 
     bool starting_page;
 
-    bool auto_cut;
-    uint8_t cut_every_x_labels;
+    std::optional<uint8_t> cut_every_x_labels;
     bool cut_at_end;
 
-    bool high_quality;
-    bool print_600dpi;
+    bool high_quality;  /**< Prefer quality over speed of printing */
 
     uint16_t margin_amount;
 
 public:
-    PrinterJobData() noexcept;
+
+    /**
+     * Default constructor that sets label properties according to
+     * values of `Label::dimensions` and `Label::type` and the rest of
+     * the class members is set to somewhat arbitrary default values.
+     *
+     * @throws std::runtime_error if `Label::is_valid()` returns `false`
+     * @see Label
+     */
+    PrinterJobData();
 
     void set_is_starting_page(bool starting_page) noexcept;
-    void set_auto_cut_options(bool auto_cut, uint8_t cut_every_x_labels = 1) noexcept;
+
+    /**
+     * Sets `cut_every_x_labels` member
+     *
+     * @param cut_every_x_labels Value in range (0, 255> or literally `{}`
+     *
+     * @throws std::invalid_argument if parameter is equal to zero
+     */
+    void set_auto_cut_options(std::optional<uint8_t> cut_every_x_labels);
+
     void set_cut_at_end(bool cut_at_end) noexcept;
     void set_quality(bool high_quality) noexcept;
 
+    /**
+     * Set `margin_amount` member.
+     *
+     * Parameter in millimeters is converted to pixels (points).
+     *
+     * @param margin_amount_mm Margin amount specified in millimeters
+     *
+     * @throws std::runtime_error if `label_type = LabelType::DIE_CUT`
+     * @throws std::invalid_argument if the parameter is not in range <3, 127>
+     */
+    void set_margin_amount(uint8_t margin_amount_mm);
+
+    /**
+     * Constructs job data message from member values.
+     *
+     * @return Vector of bytes that represents job data packet
+     */
     [[nodiscard]] std::vector<uint8_t> construct_job_data_message() const noexcept;
 };
 
